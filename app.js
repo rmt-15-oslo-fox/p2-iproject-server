802e45e44cd67dca436af719cd7757e83370cc68
Enjay Tarigan <tariganagc@gmail.com>
2021-09-20 22:55:24 +0700

    added dev depedencies: dotenv

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const router = require("./routes/index");
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
