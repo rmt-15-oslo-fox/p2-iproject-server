13f4fc03fdedf9adca23045742a1e39040636fdf
Enjay Tarigan <tariganagc@gmail.com>
2021-09-20 20:39:06 +0700

    added model and table Users to database

---
+++ models/user.js
@@ -0,0 +1,71 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Name cannot be null",
+          },
+          notEmpty: {
+            msg: "Name is required",
+          },
+          min: {
+            args: [3],
+            msg: "Your name is too short! You need 3+ characters",
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: {
+            msg: "Invalid email address",
+          },
+          notNull: {
+            msg: "Email cannot be null",
+          },
+          notEmpty: {
+            msg: "Email is required",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Password cannot be null",
+          },
+          notEmpty: {
+            msg: "Password is required",
+          },
+          min: {
+            args: [5],
+            msg: "Your password is too short! You need 5+ characters",
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
