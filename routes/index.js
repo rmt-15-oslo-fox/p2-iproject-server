a3a1b6a0655dd2dcc8909839a6d461985715e9b3
JoenathanSent <jonathansentosa80@gmail.com>
2021-09-21 15:21:09 +0700

    proses buat route user

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const userRoutes = require("./user");
+const { authentication } = require("../middleware/authentication");
+const errHandler = require("../middleware/errHandler");
+
+router.use("/", userRoutes);
+router.use(authentication);
+router.use("/list", listRoutes);
+router.use(errHandler);
+
+module.exports = router;
