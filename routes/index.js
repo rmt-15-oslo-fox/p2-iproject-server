7a575fa72e596a166f71296fda7b950b39789230
Enjay Tarigan <tariganagc@gmail.com>
2021-09-22 11:37:33 +0700

    small changes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const errorHandler = require("../middleware/error");
 const authRouter = require("./authRouter");
+const courseRouter = require("./courseRouter");
+const publicRouter = require("./publicRouter");
 const userRouter = require("./useRouter");
 const router = express.Router();
 
 router.use("/", authRouter);
+router.use("/courses", courseRouter);
 router.use("/users", userRouter);
+router.use("/pub", publicRouter);
 router.use(errorHandler);
 module.exports = router;
