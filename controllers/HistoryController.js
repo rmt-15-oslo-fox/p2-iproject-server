202acd55536904df88cf17eafca71ca9acbb65fe
dandyraspudi <dandyraspudi@gmail.com>
2021-09-22 09:39:55 +0700

    history controller done

--- controllers/HistoryController.js
+++ controllers/HistoryController.js
@@ -5,7 +5,19 @@ const {
 class HistoryController {
 
     static async getHistory(req, res, next) {
-
+        try {
+            const checkData = await History.findAll()
+            if (checkData) {
+                res.status(200).json(checkData)
+            } else {
+                next({
+                    name: "NotFound",
+                    message: "Contact not found"
+                })
+            }
+        } catch (error) {
+            next(error)
+        }
     }
 
 }
