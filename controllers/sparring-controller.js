ca6ac7d485a961cf904479c3cadd4224367aba0d
fikrialfjri <fikribusiness2021@gmail.com>
2021-09-22 17:01:39 +0700

    Add method create sparring with imageKit and method find sparring by id

--- controllers/sparring-controller.js
+++ controllers/sparring-controller.js
@@ -13,6 +13,45 @@ class SparringController {
       next(error)
     }
   }
+
+  static async findById(req, res, next) {
+    const id = req.params.id
+    try {
+      const findSparringById = await Sparring.findByPk(id)
+
+      if (findSparringById) {
+        res.status(200).json(findSparringById)
+      } else {
+        throw {
+          name: "NotFoundSparringError",
+          message: `Sparring with id ${id} not found`
+        }
+      }
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async create(req, res, next) {
+    let temp = req.body
+    const data = {
+      teamName: temp.teamName,
+      teamLogo: temp.imageUrl,
+      description: temp.description,
+      schedule: temp.schedule,
+      location: temp.location,
+      status: "Active",
+      CategoryId: temp.CategoryId,
+      AuthorId: req.user.id
+    }
+
+    try {
+      const newSparring = await Sparring.create(data)
+      res.status(201).json(newSparring)
+    } catch (error) {
+      next(error)
+    }
+  }
 }
 
 module.exports = SparringController
--- middlewares/handle-error.js
+++ middlewares/handle-error.js
@@ -1,5 +1,5 @@
 function errorHandle(err, req, res, next) {
-  console.log(err);
+  console.log(err, "<<<<<<<<<<<");
   let code = 500
   let message = "Internal server error"
 
@@ -15,6 +15,11 @@ function errorHandle(err, req, res, next) {
     case "NotMatchedUserError":
       code = 401
       message = err.message
+      break;
+    case "NotFoundSparringError":
+      code = 404
+      message = err.message
+      break;
     default:
       break;
   }
