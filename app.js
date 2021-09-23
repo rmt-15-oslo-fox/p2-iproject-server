1c11efabe9c3a6517e0510c00c9034370a97d035
Enjay Tarigan <tariganagc@gmail.com>
2021-09-23 09:01:47 +0700

    heroku cors error -_-

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const express = require("express");
-const cors = require("cors");
 const app = express();
 const router = require("./routes/index");
-app.use(cors());
+app.use(require("cors")());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
