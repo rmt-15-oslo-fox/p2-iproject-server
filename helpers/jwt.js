584eebd7abc9cd7ca48c8552bc1c6fdf6a6d2d61
kautsar_masuara <kautsarmasuara@gmail.com>
2021-09-23 10:22:36 +0800

    Production

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken')
 const getToken = (payload) => {
     return jwt.sign(payload,process.env.JWT_SECRET)
 }
-
 const decodeToken = (access_token) => {
     return jwt.verify(access_token,process.env.JWT_SECRET)
 }
--- routes/get3rd.js
+++ routes/get3rd.js
@@ -2,6 +2,9 @@ const apiRouter = require('express').Router()
 const apiController = require('../controllers/3rdParty')
 const auth = require('../middlewares/auth')
 
+apiRouter.get('/',(req,res)=> {
+    res.send('Aplikasi berjalan')
+})
 apiRouter.get('/getDataCovidIndo',apiController.getDataCovidIndo)
 apiRouter.use(auth)
 apiRouter.get('/hospital/location',apiController.getLocation)
