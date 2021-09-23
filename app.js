e1c7c3f92be7d689811b4d69acb8180b35ef305e
JoenathanSent <jonathansentosa80@gmail.com>
2021-09-23 11:44:24 +0700

    deploy heroku 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// if (process.env.NODE_ENV === "development") {
-//   require("dotenv").config();
-// }
-require("dotenv").config();
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
+// require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
