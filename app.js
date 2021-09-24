de10faa31088504e7adf2a4d5384591f02881ea9
fikrialfjri <fikribusiness2021@gmail.com>
2021-09-24 11:51:10 +0700

    Add socket io but not working

--- app.js
+++ app.js
@@ -4,6 +4,29 @@ const app = express()
 const port = process.env.PORT || 3000
 const cors = require("cors")
 const routes = require("./routes/index.js")
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer);
+
+const users = []
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  console.log(socket);
+
+  socket.on("sendMessage", (data) => {
+    console.log(data, "<<<<< server");
+
+    // io.emit("broadcastMessage", data)
+    socket.broadcast.emit("broadcastMessage", data)
+  })
+
+  socket.on("loginUser", (user) => {
+    console.log(user);
+    users.push(user)
+
+    io.emit("usersLogin", users)
+  })
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -11,6 +34,6 @@ app.use(cors())
 
 app.use(routes)
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log("App listen on port", +port);
 })
